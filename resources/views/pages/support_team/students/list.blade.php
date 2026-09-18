6af5b3834afd3f34e996fa67983d9d1c4bb9cac9
mahdimarino <mahdismahi.13@gmail.com>
2026-09-18 04:31:54 +0400

    test email course

--- resources/views/pages/support_team/students/list.blade.php
+++ resources/views/pages/support_team/students/list.blade.php
@@ -114,7 +114,7 @@
                                                         <a href="{{ route('students.edit', Qs::hash($sr->id)) }}" class="dropdown-item"><i class="icon-pencil"></i> Edit</a>
                                                         <a href="{{ route('st.reset_pass', Qs::hash($sr->user->id)) }}" class="dropdown-item"><i class="icon-lock"></i> Reset password</a>
                                                     @endif
-                                                    <a href="#" class="dropdown-item"><i class="icon-check"></i> Marksheet</a>
+                                                    {{-- <a href="#" class="dropdown-item"><i class="icon-check"></i> Marksheet</a> --}}
 
                                                     {{--Delete--}}
                                                     @if(Qs::userIsSuperAdmin())
